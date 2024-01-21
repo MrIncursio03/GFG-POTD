956cd3482ef4ae3b49d4b94dc193aee295b44d81
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2024-01-21 20:06:26 +0530

    Update 21-01-2024.cpp

--- 2024/01 January 2024/21-01-2024.cpp
+++ 2024/01 January 2024/21-01-2024.cpp
@@ -7,16 +7,6 @@ Difficulty : Hard
 
 */
 
-
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function Template for C++
-
 class Solution{
     public:
     
@@ -73,28 +63,3 @@ class Solution{
         return high;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,m;
-        cin>>n>>m;
-        vector<pair<int,int>> edges;
-        for(int i=0;i<m;i++)
-        {
-            int a,b;
-            cin>>a>>b;
-            edges.push_back({a,b});
-        }
-        Solution s;
-        cout<<s.vertexCover(n,edges)<<endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
