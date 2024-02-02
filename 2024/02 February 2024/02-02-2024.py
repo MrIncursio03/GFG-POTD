cd7d5ac035d1c53fea9c724023a211d1f1142d8c
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2024-02-02 20:58:59 +0530

    Create 02-02-2024.py

---
+++ 2024/02 February 2024/02-02-2024.py
@@ -0,0 +1,26 @@
+"""
+
+Author : Shuvam Kumar Singh
+Date : 01/02/2024
+Problem : Implement Atoi
+Difficulty : Medium
+
+"""
+
+
+class Solution:
+    def atoi(self, s: str) -> int:
+        ans = 0
+        sign = 1
+        n = len(s)
+
+        for i in range(n):
+            if s[i] == '-' and i == 0:
+                sign = -1
+            elif '0' <= s[i] <= '9':
+                ans = ans * 10 + int(s[i])
+            else:
+                return -1
+
+        ans *= sign
+        return ans
