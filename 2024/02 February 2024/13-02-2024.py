11ae9bd958dca8c7c10bab117bb4687af4ce3ff0
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2024-02-13 21:51:23 +0530

    Create 13-02-2024.py

---
+++ 2024/02 February 2024/13-02-2024.py
@@ -0,0 +1,27 @@
+"""
+
+Author : Shuvam Kumar Singh
+Date : 13/02/2024
+Problem : Clone an Undirected Graph
+Difficulty : Medium
+
+"""
+
+
+class Solution():
+    def cloneGraph(self, node):
+        #your code goes here
+        from sys import setrecursionlimit
+        setrecursionlimit(10**4)
+        corr=dict()
+        q=[node]
+        while q:
+            cur=q.pop()
+            corr[cur]=Node()
+            for nxt in cur.neighbors:
+                if nxt not in corr:
+                    q.append(nxt)
+        for ex,ne in corr.items():
+            ne.val=ex.val
+            ne.neighbors=[corr[x] for x in ex.neighbors]
+        return corr[node]
